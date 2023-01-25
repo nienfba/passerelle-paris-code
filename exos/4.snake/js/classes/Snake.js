5188ae2b5ed8d8e7700a34d2471f75a2f7c8a11f
Fabien <fab@alti-com.fr>
2023-01-25 16:06:27 +0100

    Update Snake 2

--- exos/4.snake/js/classes/Snake.js
+++ exos/4.snake/js/classes/Snake.js
@@ -1,4 +1,14 @@
 
+
+const GAME_KEY = {
+    right: 39,
+    left: 37,
+    up: 38,
+    down: 40,
+    pause: 32,
+};
+
+
 export class Snake {
 
 
@@ -8,10 +18,15 @@ export class Snake {
         */
         this.domObject = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
 
+        this.init();
+        
+        this.initSvg();
+    }
+
+    init() {
         this.x = 10;
         this.y = 10;
 
-        this.initSvg();
     }
 
     initSvg() {
@@ -21,13 +36,27 @@ export class Snake {
         this.domObject.setAttribute('x', this.x);
         this.domObject.setAttribute('y', this.y);
     }
-
+    
     getSvg() {
         return this.domObject;
     }
+    
+    move(direction) {
+        switch (direction) {
+            case GAME_KEY.right:
+                this.x += 1;
+                break;
+            case GAME_KEY.left:
+                this.x -= 1;
+                break;
+            case GAME_KEY.up:
+                this.y -=1;
+                break;
+            case GAME_KEY.down:
+                this.y += 1;
+                break;
+        }
 
-    move() {
-        this.x +=1;
         this.initSvg();
     }
 }
--- exos/4.snake/js/classes/SnakeGame.js
+++ exos/4.snake/js/classes/SnakeGame.js
@@ -7,13 +7,22 @@ const GAME_SIZE = {
     boxSizeY: 30
 };
 
+const GAME_KEY = {
+    right: 39,
+    left: 37,
+    up: 38,
+    down: 40,
+    pause: 32,
+};
+
 
 export class SnakeGame {
 
     constructor(domZoneDisplay = null) {
 
-        // Si on a une zone HTML qui est passée pour l'affichage.
-        // Sinon directement dans le body
+        /**
+         * @var {Element} domZoneDisplay Si on a une zone HTML qui est passée pour l'affichage. Sinon directement dans le body
+         */
         this.domZoneDisplay = (domZoneDisplay == null) ? document.body : domZoneDisplay;
 
         /**
@@ -21,15 +30,25 @@ export class SnakeGame {
          */
         this.domObject = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 
-
+        /**
+         * @var {Snake} snake : le serpent
+         */
         this.snake = new Snake();
+
+        /**
+         * @var {Number} identifiant du timer du jeu
+         */
+        this.intervalId = null;
+
+        // Ajout du gestionnaire d'event Clavier
+        document.addEventListener('keydown', this.keyEvent.bind(this)); 
 
         // intialisaton du programme
         // on sépare l'initialisation du contructeur pour pouvoir initialiser le jeu après un GameOver
         this.init();
 
 
-        document.addEventListener('keydown',this.keyEvent.bind(this));
+       
        
     }
 
@@ -40,45 +59,76 @@ export class SnakeGame {
          */
         this.score = 0;
 
+        /**
+         * @var {Number|null} Direction courante du snake
+         */
+        this.direction = null
+
         //On vide le SVG
         this.domObject.innerHTML = '';
 
+        //initialisation position snake
+        this.snake.init();
+
         //Creation de l'objet du DOM (balise SVG)
         this.initSvg();
+
+        
  
     }
 
+    /**
+     * Initialise / Modifie le SVG 
+     */
     initSvg() {
-
         this.domObject.setAttribute('width', GAME_SIZE.x);
         this.domObject.setAttribute('height', GAME_SIZE.y);
         this.domObject.setAttribute('viewBox', `0 0 ${GAME_SIZE.boxSizeX} ${GAME_SIZE.boxSizeY}`);
         this.domObject.style.border = '1px solid blue';
-        this.domObject.appendChild(this.snake.getSvg());
-        // on devra ajouter un serpent et une pomme
     }
 
     start() {
 
-        // on ajoute le svg à la page
+        // Ajout du serpent 
+        this.domObject.appendChild(this.snake.getSvg());
+
+        // Ajout de la pomme
+
+        // on ajoute le svg à la zone dans la page
         this.domZoneDisplay.appendChild(this.domObject);
 
         // on dit à setInterval de lier la fonction play avec le contexte défini par this. Ici this est bien l'instance de mon objet.
         // donc maintenant dans play, *this* sera toujours l'instance de l'objet !
         // COOL !
-        this.intervalId = setInterval(this.play.bind(this), 500);
+        this.intervalId = setInterval(this.play.bind(this), 200);
     }
 
     play() {
         // ça marche ici.. car *this* est bien l'instance de l'objet SnakeGame
-        this.snake.move();
+        this.snake.move(this.direction);
+
+        this.collisionOut();
+        //this.collisionApple();
+    }
+
+    collisionOut() {
+        if (this.snake.x < 0 || this.snake.y < 0 || this.snake.x == GAME_SIZE.boxSizeX || this.snake.y == GAME_SIZE.boxSizeY) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+            window.alert('Game over');
+            this.init();
+        }
     }
 
 
     keyEvent(e) {
-        console.log(e);
-        console.log(e.keycode);
-        if (e.keyCode == 32) 
+        // si on a pas démarré le timer on lance le jeu
+        if (this.intervalId == null) {
             this.start();
+            return;
+        }
+
+        if (e.keyCode == GAME_KEY.right || e.keyCode == GAME_KEY.left || e.keyCode == GAME_KEY.up || e.keyCode == GAME_KEY.down)
+            this.direction = e.keyCode;
     }
 }
